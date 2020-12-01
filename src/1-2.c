a0d6d504cea30170745e2da43a2b579f145c6aa9
山田敬汰 <banira0428@gmail.com>
2020-12-01 11:21:25 +0900

    1-2

--- src/1-2.c
+++ src/1-2.c
@@ -1,22 +1,26 @@
 #include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
+#define BUF_SIZE 64
 
 int main(int argc, char *argv[]) {
 
   // open file
-  int fd = open("data/1-1", O_RDONLY);
+  int fd = open("data/1-2", O_RDONLY);
   if(fd == -1){
     printf("failed to open\n");
     return 1;
   }
 
-  // write file
-  int result = write(fd, argv[1], strlen(argv[1]));
+  // read file
+  char buf[BUF_SIZE];
+  int result = read(fd, buf, BUF_SIZE);
   if(result == -1){
-    printf("failed to write\n");
+    printf("failed to read\n");
     return 1;
   }
+
+  // print result
+  printf("%s\n", buf);
 
   // close file
   close(fd);
